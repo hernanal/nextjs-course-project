bb45abc6cc29f9d5b236f51c67b04cbbea09cb9b
Alex Hernandez <alexh385@gmail.com>
2023-04-22 14:56:48 -0400

    feat: add event routes

---
+++ src/pages/events/[...slug].tsx
@@ -0,0 +1,13 @@
+/**
+ * This page will only be rendered if there is more than one dynamic path param pass in
+ * I.e. events/1/2
+ * */
+const FilteredEventsPage = () => {
+  return (
+    <div>
+      <h1>Filtered Events</h1>
+    </div>
+  )
+}
+
+export default FilteredEventsPage
