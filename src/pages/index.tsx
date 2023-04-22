03a81ae2b31d85d58dc22f2f2f034f05edf8da55
Alex Hernandez <alexh385@gmail.com>
2023-04-21 22:38:18 -0400

    refactor: format home and about pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 const HomePage = () => {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  );
-};
+    return (
+        <div>
+            <h1>Home</h1>
+        </div>
+    )
+}
 
-export default HomePage;
+export default HomePage
