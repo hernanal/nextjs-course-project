d92f142fd8257e2cf490e57f7cd79b32e0223d81
Alex Hernandez <alexh385@gmail.com>
2023-04-22 14:42:39 -0400

    refactor: remove old routes to get ready for the new project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,14 +4,6 @@ const HomePage = () => {
   return (
     <div>
       <h1>Home</h1>
-      <ul>
-        <li>
-          <Link href="/portfolio">Portfolio</Link>
-        </li>
-        <li>
-          <Link href="/clients">Clients</Link>
-        </li>
-      </ul>
     </div>
   )
 }
