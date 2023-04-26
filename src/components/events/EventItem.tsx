ada393a5fca399b2173c54f8121e4fe732158a28
Alex Hernandez <alexh385@gmail.com>
2023-04-26 00:31:26 -0400

    feat: add Button component, Icons, and implement them in EventItem

--- src/components/events/EventItem.tsx
+++ src/components/events/EventItem.tsx
@@ -1,6 +1,9 @@
 import { Event } from '@/types/eventTypes'
-import Link from 'next/link'
 import classes from './event-item.module.css'
+import Button from '../ui/Button'
+import DateIcon from '../icons/DateIcon'
+import AddressIcon from '../icons/AddressIcon'
+import ArrowRightIcon from '../icons/ArrowRightIcon'
 
 interface EventProps {
   event: Event
@@ -22,14 +25,21 @@ const EventItem = ({ event }: EventProps) => {
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Link href={exploreLink}>Explore Event</Link>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
