45c66989f3b6bda9d4a26ffd061ee5cfd460efdf
Alex Hernandez <alexh385@gmail.com>
2023-05-05 01:38:28 -0400

    feat: add SSG to Home page and implement new api util function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,29 @@
-import { getAllEvents } from '@/dummy-data'
 import EventList from '@/components/events/EventList'
+import { getFeaturedEvents } from '@/helpers/apiUtils'
+import { Event } from '@/types/eventTypes'
 
-const HomePage = () => {
-  const featuredEvents = getAllEvents()
+interface HomePageProps {
+  events: Event[]
+}
 
+const HomePage = (props: HomePageProps) => {
+  const { events } = props
   return (
     <div>
-      <EventList events={featuredEvents} />
+      <EventList events={events} />
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents()
+
+  return {
+    props: {
+      events: featuredEvents,
+    },
+    revalidate: 1800,
+  }
 }
 
 export default HomePage
