76e46384c2b45b4e8b4002ab53a4044c9b7ae3e9
Alex Hernandez <alexh385@gmail.com>
2023-05-05 23:31:20 -0400

    feat: add getFilteredEvents to apiUtils

--- src/helpers/apiUtils.ts
+++ src/helpers/apiUtils.ts
@@ -25,3 +25,18 @@ export async function getEventById(id: string | string[] | undefined) {
   const allEvents = await getAllEvents()
   return allEvents.find((event) => event.id === id)
 }
+
+export async function getFilteredEvents(dateFilter: any) {
+  const { year, month } = dateFilter
+
+  const allEvents = await getAllEvents()
+
+  let filteredEvents = allEvents.filter((event) => {
+    const eventDate = new Date(event.date)
+    return (
+      eventDate.getFullYear() === year && eventDate.getMonth() === month - 1
+    )
+  })
+
+  return filteredEvents
+}
