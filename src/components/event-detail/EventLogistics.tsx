6b1921b5fd00823844ecacf8c7bde33ab3bbf6bb
Alex Hernandez <alexh385@gmail.com>
2023-05-07 11:51:47 -0400

    feat: replace img element with nextjs Image component to optimize the images in the app

--- src/components/event-detail/EventLogistics.tsx
+++ src/components/event-detail/EventLogistics.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import AddressIcon from '../icons/AddressIcon'
 import DateIcon from '../icons/DateIcon'
 import LogisticsItem from './LogisticsItem'
@@ -23,7 +24,7 @@ function EventLogistics(props: EventLogisticsProps) {
   return (
     <section className={classes.logistics}>
       <div className={classes.image}>
-        <img src={`/${image}`} alt={imageAlt} />
+        <Image src={`/${image}`} alt={imageAlt} width={400} height={400} />
       </div>
       <ul className={classes.list}>
         <LogisticsItem icon={DateIcon}>
