4e2828b680bda548df3807a9cb7bc4b9248e6837
Alex Hernandez <alexh385@gmail.com>
2023-05-06 22:52:01 -0400

    feat: add default Head title and meta data, just in case a page isn't setting this info already

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,8 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Head key="app-head">
+        <title>Next Events</title>
+        <meta name="description" content="NextJS Events." />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
